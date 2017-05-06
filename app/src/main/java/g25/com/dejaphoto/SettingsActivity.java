550a5ccc9bb8b86dc751d00e68bc026a7051501d
Tim Lu <tylu@ucsd.edu>
2017-05-06 12:54:40 -0700

    Using SharedPreferences to store album choice

--- app/src/main/java/g25/com/dejaphoto/SettingsActivity.java
+++ app/src/main/java/g25/com/dejaphoto/SettingsActivity.java
@@ -1,15 +1,34 @@
 package g25.com.dejaphoto;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "DejaPhotoPreferences";
+    private boolean useCustomAlbum;
+    private SharedPreferences settings;
+    private SharedPreferences.Editor settingsEditor;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        int v = 50;
+
+        //initialize fields
+        settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        settingsEditor = settings.edit();
+        useCustomAlbum = settings.getBoolean("useCustomAlbum", false);
+    }
+
+    public void selectDefaultAlbum(View view){
+        settingsEditor.putBoolean("useCustomAlbum", false);
+        settingsEditor.commit();
+    }
+
+    public void selectCustomAlbum(View view){
+        settingsEditor.putBoolean("useCustomAlbum", true);
+        settingsEditor.commit();
     }
 }
 
