8f773bd922c415a5330668dd156cd127871439f6
Tim Lu <tylu@ucsd.edu>
2017-05-06 19:12:32 -0700

    Switched to AlarmManager

---
+++ app/src/main/java/g25/com/dejaphoto/AlarmReceiver.java
@@ -0,0 +1,25 @@
+package g25.com.dejaphoto;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Tim on 5/6/2017.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    static WallpaperChanger wallpaperChanger;
+    @Override
+    public void onReceive(Context context, Intent intent){
+        if(wallpaperChanger == null){
+            wallpaperChanger = new WallpaperChanger(context);
+            wallpaperChanger.initialize();
+        }
+        else
+            wallpaperChanger.next();
+        Log.e("ALARMRECEIVER", "RECEIVED");
+    }
+}
