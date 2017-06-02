2ec99f5719a55c08eb5883776b7e05a798c86152
Angela Zhang <angelazhang@Angelas-MacBook-Pro-3.local>
2017-06-02 14:47:53 -0700

    selection for dejaphotocopied works, but not copied to album yet

--- app/src/main/java/g25/com/dejaphoto/AlbumsActivity.java
+++ app/src/main/java/g25/com/dejaphoto/AlbumsActivity.java
@@ -2,12 +2,98 @@ package g25.com.dejaphoto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import com.esafirm.imagepicker.features.ImagePicker;
+import com.esafirm.imagepicker.features.camera.CameraModule;
+import com.esafirm.imagepicker.features.camera.ImmediateCameraModule;
+import com.esafirm.imagepicker.features.camera.OnImageReadyListener;
+import com.esafirm.imagepicker.model.Image;
 
 public class AlbumsActivity extends AppCompatActivity {
+
+    private static final int RC_CODE_PICKER = 2000;
+    private static final int RC_CAMERA = 3000;
+
+    private TextView textView;
+    private ArrayList<Image> images = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_albums);
+
+        textView = (TextView) findViewById(R.id.text_view);
+
+        findViewById(R.id.button_pick_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start();
+            }
+        });
     }
+
+    public void start() {
+        //final boolean returnAfterCapture = ((Switch) findViewById(R.id.ef_switch_return_after_capture)).isChecked();
+        //final boolean isSingleMode = ((Switch) findViewById(R.id.ef_switch_single)).isChecked();
+        //final boolean useCustomImageLoader = ((Switch) findViewById(R.id.ef_switch_imageloader)).isChecked();
+
+        ImagePicker imagePicker = ImagePicker.create(this)
+                //.theme(R.style.ImagePickerTheme)
+                //.returnAfterFirst(returnAfterCapture) // set whether pick action or camera action should return immediate result or not. Only works in single mode for image picker
+                .folderMode(true) // set folder mode (false by default)
+                .folderTitle("Folder") // folder selection title
+                .imageTitle("Tap to select"); // image selection title
+
+        /*if (useCustomImageLoader) {
+            imagePicker.imageLoader(new GrayscaleImageLoader());
+        }
+
+        if (isSingleMode) {
+            imagePicker.single();
+        } else {*/
+            imagePicker.multi(); // multi mode (default mode)
+        //}
+
+        imagePicker.limit(10) // max images can be selected (99 by default)
+                .showCamera(true) // show camera or not (true by default)
+                .imageDirectory("Camera")   // captured image directory name ("Camera" folder by default)
+                .origin(images) // original selected images, used in multi mode
+                .start(RC_CODE_PICKER); // start image picker activity with request code
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
+        if (requestCode == RC_CODE_PICKER && resultCode == RESULT_OK && data != null) {
+            images = (ArrayList<Image>) ImagePicker.getImages(data);
+            printImages(images);
+            return;
+        }
+
+        /*if (requestCode == RC_CAMERA && resultCode == RESULT_OK) {
+            getCameraModule().getImage(this, data, new OnImageReadyListener() {
+                @Override
+                public void onImageReady(List<Image> resultImages) {
+                    images = (ArrayList<Image>) resultImages;
+                    printImages(images);
+                }
+            });
+        }*/
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void printImages(List<Image> images) {
+        if (images == null) return;
+
+        StringBuilder stringBuffer = new StringBuilder();
+        for (int i = 0, l = images.size(); i < l; i++) {
+            stringBuffer.append(images.get(i).getPath()).append("\n");
+        }
+        textView.setText(stringBuffer.toString());
+    }
+
+
 }
