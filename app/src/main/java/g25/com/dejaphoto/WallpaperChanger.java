a8e7e349fb7c3df758fb792da8618b7e04254e46
bigmak712 <bigmak712@gmail.com>
2017-06-09 22:59:14 -0700

    First attempt at resizing wallpaper

--- app/src/main/java/g25/com/dejaphoto/WallpaperChanger.java
+++ app/src/main/java/g25/com/dejaphoto/WallpaperChanger.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.location.Address;
@@ -43,6 +44,10 @@ public class WallpaperChanger {
     static SharedPreferences settings;
     static SharedPreferences.Editor settingsEditor;
     public PhotoCompare photoCompare;
+    private int display_width = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int display_height = Resources.getSystem().getDisplayMetrics().heightPixels;
+    private int display_ratio = display_height/display_width;
+
     /**
      * Constructor passes in activity to get context and stuff
      */
@@ -66,10 +71,23 @@ public class WallpaperChanger {
      */
     private void setWallpaper(BackgroundPhoto photoWrapper) {
         Uri uri = photoWrapper.getUri();
+
         try {
             myWallpaperManager = WallpaperManager.getInstance(context);
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
             //Bitmap b = addLocationtoBitmap(bitmap);
+
+            int bitmap_ratio = bitmap.getHeight()/bitmap.getWidth();
+
+            if(bitmap.getWidth() > display_width) {
+                bitmap.setHeight(bitmap_ratio * display_width);
+                bitmap.setWidth(display_width);
+            }
+
+            if(bitmap.getHeight() > display_height) {
+                bitmap.setWidth(display_height / bitmap_ratio);
+                bitmap.setHeight(display_height);
+            }
 
             myWallpaperManager.setBitmap(bitmap);
         } catch (IOException e) {
@@ -77,7 +95,6 @@ public class WallpaperChanger {
         }
         Log.e("POINTS", Integer.toString(photoWrapper.getPoints()));
     }
-
 
     /**
      * Cursor gets the photos from media store and we use it to point to each photo in
