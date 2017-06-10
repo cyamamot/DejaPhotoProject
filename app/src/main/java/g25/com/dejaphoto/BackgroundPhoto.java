f471ef3aea635695838341479e4cc6825926cf85
bigmak712 <bigmak712@gmail.com>
2017-06-09 23:36:40 -0700

    fixing merge conflict

--- app/src/main/java/g25/com/dejaphoto/BackgroundPhoto.java
+++ app/src/main/java/g25/com/dejaphoto/BackgroundPhoto.java
@@ -371,7 +371,7 @@ public class BackgroundPhoto {
             Log.e("BackgroundPhoto", "k2");
             return;
         }
-        if(hasKarma()){
+        if(hasKarma() && listOfKarmaers.contains(id)){
             Log.e("BackgroundPhoto", "k3");
             return;
         }
